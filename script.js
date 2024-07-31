032c9655e1186736cddb4f6f6104bd1f16e9e986
Victor-lyhan <109762958+Victor-lyhan@users.noreply.github.com>
2024-07-31 15:55:16 -0500

    dark light mode, scroll review

--- script.js
+++ script.js
@@ -27,4 +27,19 @@ let darkModeIcon = document.querySelector('#darkMode-icon')
 
 darkModeIcon.onclick = () => {
     darkModeIcon.classList.toggle('bx-sun');
+    document.body.classList.toggle('dark-mode');
 }
+
+/*======== scroll review ========*/
+ScrollReveal({
+    reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
+
+ScrollReveal().reveal('.home-content, .heading', {origin: 'top'});
+ScrollReveal().reveal('.home-img img, .experiences.container, .awards-box, .contact form'
+    , {origin: 'bottom'});
+ScrollReveal().reveal('.home-content h1, .about-img img' , {origin: 'left'});
+ScrollReveal().reveal('.home-content h3, .home-content p, .about-content' , {origin: 'right'});
